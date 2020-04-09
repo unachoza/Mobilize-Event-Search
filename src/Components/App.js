34c32429849417ee36602c6db353c6ad418a6d11
arianna choza <unachoza@gmail.com>
2020-04-08 22:43:16 -0400

    react set up

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>Search Mobilze API for events displayed on map</div>;
+  }
+}
+
+export default App;
