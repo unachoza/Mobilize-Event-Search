60205eef0c8bad4dc20075ea46f59493fbb56648
arianna choza <unachoza@gmail.com>
2020-04-10 17:39:57 -0400

    data normalized while using eventsfetchhook

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,7 @@ import LoadingSpinner from 'Components/loadingSpinner/loadingSpinner.component';
 const MOBILZE_BASE_URL = 'https://api.mobilize.us/v1/events?per_page=4';
 
 const App = () => {
-  const fetchedEvents = useEventsFetch(MOBILZE_BASE_URL, { fetchedEvents: null, loading: true });
+  let fetchedEvents = useEventsFetch(MOBILZE_BASE_URL, { fetchedEvents: null, loading: true });
   const [requestURL, setRequestURL] = useState(MOBILZE_BASE_URL);
   const [loading, setloading] = useState(false);
   console.log(fetchedEvents, loading);
@@ -21,15 +21,35 @@ const App = () => {
   if (!fetchedEvents || fetchedEvents.loading) {
     return <LoadingSpinner loading={loading} />;
   } else if (fetchedEvents.fetchedEvents.data) {
-    
-    
-    
-    console.log(fetchedEvents.fetchedEvents.data)
+    fetchedEvents = fetchedEvents.fetchedEvents.data.map((event) => {
+      return {
+        id: event.id,
+        eventType: event.event_type,
+        title: event.title,
+        details: event.description,
+        zipcode: event.location?.postal_code || null,
+        coordinates: {
+          lat: event.location?.location?.latitude || null,
+          lng: event.location?.location?.longitude || null,
+        },
+        eventDate: {
+          start: event.timeslots[0]?.start_date || null,
+          end_date: event.timeslots[0]?.end_date || null,
+        },
+        url: event.browser_url || null,
+        eventImg: event.featured_image_url || null,
+        show: false,
+      };
+    });
+   
+   
+  
+    console.log(fetchedEvents);
     return (
       <div>
         <Header />
         <div className="body">
-          <EventsContext.Provider value={fetchedEvents.fetchedEvents.data}>
+          <EventsContext.Provider value={fetchedEvents}>
             {/* {!fetchedEvents.props?.children ? ( */}
             <EventList events={fetchedEvents} loading={loading} />
 
@@ -64,3 +84,36 @@ export default App;
 // const upDateRequestUrl = (input, moreInputs = '') => {
 //   setRequestURL(MOBILZE_BASE_URL + '&zipcode=' + input + moreInputs);
 // };
+ // ****************************
+    //  const fetchedEvents = await data.data.map((event) => {
+    //       return {
+    //         id: event.id,
+    //         eventType: event.event_type,
+    //         title: event.title,
+    //         details: event.description,
+    //         zipcode: event.location?.postal_code || null,
+    //         coordinates: {
+    //           lat: event.location?.location?.latitude || null,
+    //           lng: event.location?.location?.longitude || null,
+    //         },
+    //         eventDate: {
+    //           start: event.timeslots[0]?.start_date || null,
+    //           end_date: event.timeslots[0]?.end_date || null,
+    //         },
+    //         url: event.browser_url || null,
+    //         eventImg: event.featured_image_url || null,
+    //         show: false,
+    //       };
+    //     });
+
+
+// ******************
+ // return fetchedEvents;
+    //   } catch (error) {
+    //     const errorMessage = 'zipcode not valid';
+    //     setError(errorMessage);
+    //     console.log(error);
+    //     return <div style={{ color: 'red', fontSize: '50px' }}>{error}</div>;
+    //     // this.catchError()
+    //   }
+    // }
