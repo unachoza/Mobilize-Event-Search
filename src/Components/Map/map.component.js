2df8700d591c79d7bbc7e01d4644618c5a071ada
arianna choza <unachoza@gmail.com>
2020-04-13 10:15:09 -0400

    marker & info box work, need to clean everything

--- src/Components/Map/map.component.js
+++ src/Components/Map/map.component.js
@@ -1,28 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import 'Components/Map/map.styles.css';
 import EventsContext from 'Context/Events/event.context';
 import { showEventDetails } from 'API/MobilizeFetch';
-
+import { useMapMarker } from 'Hooks/mapMarkers.hook';
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 // https://api.mobilize.us/v1/events?per_page=10&zipcode=11222
 // https://api.mobilize.us/v1/events/82193
 
+const EventMarker = () => {
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  useEffect(() => {
+    const showMarkerInfoBox = (event) => {
+      showEventDetails(event.id);
+      console.log('i was clickedc', event);
+      return event;
+    };
+  }, [selectedMarker]);
 
+  return (
+    <EventsContext.Consumer>
+      {(fetchedEvents) =>
+        fetchedEvents.map((event, i) => {
+          return (
+            <div key={i}>
+              <Marker
+                key={event.id}
+                // onClick={() => setSelectedMarker(event)}
+                // onClick={(e) => showEventDetails(event.id)}
+                onClick={() => console.log(setSelectedMarker(event.id))}
+                markers={event.title}
+                position={{
+                  lat: event.coordinates.lat,
+                  lng: event.coordinates.lng,
+                }}
+              />
+              {selectedMarker && (
+                <InfoWindow
+                  position={{
+                    lat: event.coordinates.lat,
+                    lng: event.coordinates.lng,
+                  }}
+                  onCloseClick={() => setSelectedMarker(null)}
+                >
+                  <div>
+                    <h2>{event.title}</h2>
+                  </div>
+                </InfoWindow>
+              )}
+            </div>
+          );
+        })
+      }
+    </EventsContext.Consumer>
+  );
+};
 
 const Map = () => {
-  const [selectedMarker, setSelectedMarker] = useState(null);
-
-  const useEffect =(() => {
-      const showMarkerInfoBox = (event) => {
-        showEventDetails(event.id);
-        console.log('i was clickedc', event);
-        setSelectedMarker(event.id);
-        return event;
-      };
-    },
-    []);
-
   return (
     <div className="map-container">
       <LoadScript id="script-loader" googleMapsApiKey={API_KEY}>
@@ -42,15 +76,25 @@ const Map = () => {
             lng: -73.9506774,
           }}
         >
-          <EventsContext.Consumer>
+          <EventMarker />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default Map;
+
+{
+  /* <EventsContext.Consumer>
             {(fetchedEvents) =>
-              fetchedEvents.map((event) => {
+              fetchedEvents.map((event, i) => {
                 return (
-                  <div>
+                  <div key={i}>
                     <Marker
                       key={event.id}
                       onClick={() => setSelectedMarker(event)}
-                      onClick={(e) => showEventDetails(event.id)}
+                      // onClick={(e) => showEventDetails(event.id)}
                       markers={event.title}
                       position={{
                         lat: event.coordinates.lat,
@@ -70,21 +114,5 @@ const Map = () => {
                 );
               })
             }
-          </EventsContext.Consumer>
-        </GoogleMap>
-      </LoadScript>
-    </div>
-  );
-};
-
-// const MapWrapped = LoadScript(GoogleMap(Map));
-
-// const MapWithMarkersAndInfoBox = () => {
-
-//   return (
-// <MapWrapped/>
-// )
-
-// }
-
-export default Map;
+          </EventsContext.Consumer> */
+}
