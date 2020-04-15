cd23d7d2bf030c12c77ef668d1cbc27c39f76c00
arianna choza <unachoza@gmail.com>
2020-04-15 12:16:16 -0400

    fixed toggle display details

--- src/Components/Event/event.component.js
+++ src/Components/Event/event.component.js
@@ -3,32 +3,33 @@ import EventTag from 'Components/EventTag/eventTag.component';
 import 'Components/Event/event.styles.css';
 
 const Event = (props) => {
-  const [displayDetails, setDisplayDetails] = useState(false)
+  const [displayDetails, setDisplayDetails] = useState(false);
 
   const getDate = (unixTimestamp) => {
     const dateObject = new Date(unixTimestamp * 1000).toLocaleString();
     return dateObject.replace(':00', '');
-    
+
     //how to get current events//
-    //when was it updated since, 
+    //when was it updated since,
     //reoccuring
     //todays unix time stamp
-    
   };
- 
-    const { title, details, eventDate, eventType } = props.event[1];
-
-    return (
-      <div className="event-card" onClick={() => setDisplayDetails(prevState => !prevState.displayDetails)}>
-        <div className="event-card__date">{getDate(eventDate.start)} </div>
-        <div className="event-card__title" onClick={() => setDisplayDetails(prevState => !prevState.displayDetails)}>
-          {title.toUpperCase()}
-        </div>
- {displayDetails ? <div className="event-card__description">{details}</div> : null} 
-        <EventTag tag={eventType} />
+  const toggleDisplayDetails = () => {
+    setDisplayDetails(!displayDetails);
+  };
+  const { title, details, eventDate, eventType } = props.event[1];
+  console.log('re rendering', displayDetails);
+  return (
+    <div className="event-card" onClick={toggleDisplayDetails}>
+      <div className="event-card__date">{getDate(eventDate.start)} </div>
+      <div className="event-card__title" onClick={toggleDisplayDetails}>
+        {title.toUpperCase()}
       </div>
-    );
-}
+      {displayDetails ? <div className="event-card__description">{details}</div> : null}
+      <EventTag tag={eventType} />
+    </div>
+  );
+};
 
 export default Event;
 
